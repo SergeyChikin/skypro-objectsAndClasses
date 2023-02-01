37a5aebd4ea8b116351e5b859c0e65140b692d39
Sergey <sergeychikin1981@gmail.com>
2023-02-01 23:01:25 +0300

    Выполнил ДЗ по объектам и классам.( Случайно удалил объект )

--- src/skyproCourse1objectsAndClasses/Main.java
+++ src/skyproCourse1objectsAndClasses/Main.java
@@ -5,6 +5,7 @@ public class Main {
         Author maksimGorkiy = new Author("Максим",  "Горький");
         Author korneyChukovskiy = new Author("Корней", "Чуковский");
         Book barmaley = new Book("БАРМАЛЕЙ", korneyChukovskiy, 1925);
+        Book mother = new Book("МАТЬ", maksimGorkiy, 1906);
 
         // Пробовал вывести в консоль для самопроверки.
 //        System.out.println(barmaley.getAuthor().getName());
