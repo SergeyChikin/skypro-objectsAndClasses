1148813c6bcffae469aca2c7341f8b0a2c5a7211
Sergey <sergeychikin1981@gmail.com>
2023-02-01 22:42:37 +0300

    Выполнил ДЗ по объектам и классам.

--- src/skyproCourse1objectsAndClasses/Main.java
+++ src/skyproCourse1objectsAndClasses/Main.java
@@ -1,4 +1,15 @@
 package skyproCourse1objectsAndClasses;
 
 public class Main {
+    public static void main(String[] args) {
+        Author maksimGorkiy = new Author("Максим",  "Горький");
+        Author korneyChukovskiy = new Author("Корней", "Чуковский");
+        Book barmaley = new Book("БАРМАЛЕЙ", korneyChukovskiy, 1925);
+
+        // Пробовал вывести в консоль для самопроверки.
+//        System.out.println(barmaley.getAuthor().getName());
+//        System.out.println(barmaley.getAuthor().getSurname());
+//        System.out.println(barmaley.getBookName());
+//        System.out.println(barmaley.getPublishingYear());
+    }
 }
